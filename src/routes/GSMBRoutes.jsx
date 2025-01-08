87e02ebae649e9094321a54f7f24d2e5ff25729e
Sadeep Chathushan <chathushan43sadeep@gmail.com>
2025-01-08 14:24:00 +0530

    GSMB router add some some router path

--- src/routes/GSMBRoutes.jsx
+++ src/routes/GSMBRoutes.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Dashboard from '../pages/GSMBOfficer/Dashboard';
+import AddNewLicense from '../pages/GSMBOfficer/AddNewLicense';
+import RegNewowner from '../pages/GSMBOfficer/RegNewOwner';
+
 
 const GSMBRoutes = () => (
   <Routes>
     <Route path="dashboard" element={<Dashboard />} />
+    <Route path="add-new-license" element={<AddNewLicense />} />
+    <Route path="register-new-owner" element={<RegNewowner />} />
 
   </Routes>
 );
