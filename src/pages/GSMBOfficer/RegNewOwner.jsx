2d1e4facb5fa382914ec86251a3d4565e7aa2a94
Sadeep Chathushan <chathushan43sadeep@gmail.com>
2025-01-08 14:23:32 +0530

    RegNew owner page create

---
+++ src/pages/GSMBOfficer/RegNewOwner.jsx
@@ -0,0 +1,138 @@
+import React from "react";
+import { Form, Input, Button, Row, Col, Upload, message } from "antd";
+import { ArrowLeftOutlined } from "@ant-design/icons";
+import { UploadOutlined } from '@ant-design/icons';
+
+const NewLicenseForm = () => {
+  const [form] = Form.useForm();
+
+  const onFinish = (values) => {
+    console.log("Form values: ", values);
+  };
+
+  const handleUploadChange = (info) => {
+    if (info.file.status === 'done') {
+      message.success(`${info.file.name} file uploaded successfully`);
+    } else if (info.file.status === 'error') {
+      message.error(`${info.file.name} file upload failed.`);
+    }
+  };
+
+  return (
+    <div
+      style={{
+        maxWidth: "800px",
+        margin: "0 auto",
+        padding: "20px",
+        background: "#f0f2f5",
+        borderRadius: "10px",
+      }}
+    >
+      <Button
+        type="link"
+        icon={<ArrowLeftOutlined />}
+        style={{ marginBottom: "16px", paddingLeft: 0, color: "#000000" }}
+        href="/gsmb/dashboard"
+      ></Button>
+
+      <h2 style={{ textAlign: "center", fontWeight: "bold", color: "#1a1a1a" }}>
+        Register New  License Owner
+      </h2>
+      <Form
+        form={form}
+        layout="vertical"
+        onFinish={onFinish}
+        style={{ gap: "16px" }}
+      >
+        <Row gutter={[16, 16]}>
+          <Col xs={24} sm={24} md={12}>
+            <Form.Item
+              label="Owner Name"
+              name="ownerName"
+              rules={[{ required: true, message: "Please input the owner name!" }]}
+            >
+              <Input />
+            </Form.Item>
+          </Col>
+          <Col xs={24} sm={24} md={12}>
+            <Form.Item
+              label="Mobile Number"
+              name="mobile"
+              rules={[{ required: true, message: "Please input the mobile number!" }]}
+            >
+              <Input />
+            </Form.Item>
+          </Col>
+          
+          <Col xs={24} sm={24} md={12}>
+            <Form.Item
+              label="Vehicle Number"
+              name="vehicleNumber"
+              rules={[{ required: true, message: "Please input the vehicle number!" }]}
+            >
+              <Input />
+            </Form.Item>
+          </Col>
+          <Col xs={24} sm={24} md={12}>
+            <Form.Item
+              label="Capacity (Cubes)"
+              name="capacity"
+              rules={[{ required: true, message: "Please input the capacity!" }]}
+            >
+              <Input />
+            </Form.Item>
+          </Col>
+          <Col xs={24} sm={24} md={12}>
+            <Form.Item
+              label="Lorry Book"
+              name="lorryBook"
+              rules={[{ required: true, message: "Please upload the lorry book image!" }]}
+            >
+              <Upload
+                name="lorryBook"
+                showUploadList={false}
+                action="/upload.do"
+                onChange={handleUploadChange}
+              >
+                <Button icon={<UploadOutlined />}>Upload Lorry Book</Button>
+              </Upload>
+            </Form.Item>
+          </Col>
+          <Col xs={24} sm={24} md={12}>
+            <Form.Item
+              label="Lorry"
+              name="lorry"
+              rules={[{ required: true, message: "Please upload the lorry image!" }]}
+            >
+              <Upload
+                name="lorry"
+                showUploadList={false}
+                action="/upload.do"
+                onChange={handleUploadChange}
+              >
+                <Button icon={<UploadOutlined />}>Upload Lorry Image</Button>
+              </Upload>
+            </Form.Item>
+          </Col>
+          <Col xs={24}>
+            <Form.Item>
+              <Button
+                type="primary"
+                htmlType="submit"
+                style={{
+                  backgroundColor: "#b51c1c",
+                  borderColor: "#b51c1c",
+                  width: "100%",
+                }}
+              >
+                Create New license Owner
+              </Button>
+            </Form.Item>
+          </Col>
+        </Row>
+      </Form>
+    </div>
+  );
+};
+
+export default NewLicenseForm;
