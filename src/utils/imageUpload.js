e8097ce39286ed2ce2d2bfb172871af4fa9fb00a
sahanthathsara1 <sahanthathsaramasakorala@gmail.com>
2025-01-10 13:27:05 +0530

    Add Supabase API key and URl for Utils ⚙️

--- src/utils/imageUpload.js
+++ src/utils/imageUpload.js
@@ -1,7 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const key = ""
-const url = ""
+const key = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1zemZ1anNlY2VveHd5d2RmdWxqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY0ODU3OTEsImV4cCI6MjA1MjA2MTc5MX0.QtDzZZvzfzDSpTDERfZzRCBXw5IcJibx7g0oVM-SQ3g`;
+const url = "https://mszfujseceoxwywdfulj.supabase.co";
+
 
 export default function uploadMediaToSupabase(file){
     return new Promise((resolve, reject) =>{
