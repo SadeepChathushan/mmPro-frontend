63276134415841aefb9ee444ae290f8a600ad928
sahanthathsara1 <sahanthathsaramasakorala@gmail.com>
2025-01-30 09:45:54 +0530

    create a model for diapatch pop up

--- src/pages/MLOwner/DispatchLoadPage.jsx
+++ src/pages/MLOwner/DispatchLoadPage.jsx
@@ -20,6 +20,7 @@ import { useLocation } from "react-router-dom";
 import dayjs from "dayjs";
 import "../../styles/MLOwner/DispatchLoadPage.css";
 import { fetchIssues, updateIssue, createIssue } from '../../services/MLOService';
+import Modals from "./Modals";
 const { Content } = Layout;
 const { Title } = Typography;
 
@@ -573,136 +574,23 @@ const DispatchLoadPage = () => {
             </Button>
           </Col>
         </Row>
-
-        {/* Success Modal */}
-               <Modal
-                 visible={isModalVisible}
-                 onCancel={() => resetFormdata()}
-                 footer={null}
-                 style={{ textAlign: "center" }}
-                 bodyStyle={{ backgroundColor: "rgba(0, 0, 0, 0.1)" }}
-               >
-                 <div style={{ fontSize: "40px", color: "brown" }}>
-                   <IoIosDoneAll />
-                 </div>
-                 <p>
-                   {language === "en"
-                     ? "Dispatched Successfully!"
-                     : language === "si"
-                     ? "සාර්ථකයි!"
-                     : "வெற்றிகரமாக அனுப்பப்பட்டது!"}
-                 </p>
-                 <Button
-                   type="primary"
-                   onClick={handleBackToHome}
-                   style={{
-                     backgroundColor: "#FFA500",
-                     color: "white",
-                     borderColor: "#FFA500",
-                     marginRight: "20px",
-                   }}
-                 >
-                   {language === "en"
-                     ? "Back to Home"
-                     : language === "si"
-                     ? "ආපසු"
-                     : "முகப்புக்குத் திரும்பு"}
-                 </Button>
-       
-                 <Button
-                   type="default"
-                   onClick={handlePrintReceipt}
-                   style={{
-                     backgroundColor: "#781424",
-                     color: "white",
-                     marginLeft: "20px",
-                   }}
-                 >
-                   {language === "en"
-                     ? "Print Receipt"
-                     : language === "si"
-                     ? "රිසිට් පත මුද්‍රණය කරන්න"
-                     : "அச்சு ரசீது"}
-                 </Button>
-               </Modal>
-       
-               {/* unSuccess Modal */}
-               <Modal
-                 visible={isProErrModalVisible}
-                 onCancel={() => setIsProErrModalVisible(false)}
-                 footer={null}
-                 style={{ textAlign: "center" }}
-                 bodyStyle={{ backgroundColor: "rgba(0, 0, 0, 0.1)" }}
-               >
-                 <div style={{ fontSize: "40px", color: "brown" }}>
-                   <IoIosCloseCircle />
-                 </div>
-                 <p>
-                   {language === "en"
-                     ? "Dispatched Unsuccessfully!"
-                     : language === "si"
-                     ? "අසාර්ථකයි!"
-                     : "அனுப்பப்பட்டது தோல்வி!"}
-                 </p>
-               </Modal>
-       
-               {/*req Error Modal */}
-               <Modal
-                 visible={isErrModalVisible}
-                 onCancel={() => setIsErrModalVisible(false)}
-                 footer={null}
-                 style={{ textAlign: "center" }}
-                 bodyStyle={{ backgroundColor: "rgba(0, 0, 0, 0.1)" }}
-               >
-                 <div style={{ fontSize: "40px", color: "brown" }}>
-                   <IoIosCloseCircle />
-                 </div>
-                 <h3>
-                   {language === "en"
-                     ? "All field are required !"
-                     : language === "si"
-                     ? "සියලුම ක්ෂේත්ර අවශ්ය වේ !"
-                     : "அனைத்து துறைகளும் தேவை!"}
-                 </h3>
-               </Modal>
-       
-               {/*cube re Error Modal */}
-               <Modal
-                 visible={isContErrModalVisible}
-                 onCancel={() => setIsContErrModalVisible(false)}
-                 footer={null}
-                 style={{ textAlign: "center" }}
-                 bodyStyle={{ backgroundColor: "rgba(0, 0, 0, 0.1)" }}
-               >
-                 <div style={{ fontSize: "40px", color: "brown" }}>
-                   <IoIosCloseCircle />
-                 </div>
-                 <h3>
-                   {language === "en"
-                     ? `Not enough cubes available. Please adjust the quantity.`
-                     : language === "si"
-                     ? "්අවශ්‍ය ප්‍රමාණය නොමැත. ප්‍රමාණය වෙනස් කරන්න්."
-                     : "போதுமான க்யூப்ஸ் கிடைக்கவில்லை. அளவை சரிசெய்யவும்."}
-                 </h3>
-               </Modal>
-               <Modal
-                 visible={isLoyalErrModalVisible}
-                 onCancel={() => setIsLoyalErrModalVisible(false)}
-                 footer={null}
-                 style={{ textAlign: "center" }}
-                 bodyStyle={{ backgroundColor: "rgba(0, 0, 0, 0.1)" }}
-               >
-                 <div style={{ fontSize: "40px", color: "brown" }}>
-                   <IoIosCloseCircle />
-                 </div>
-                 <h3>
-                   {language === "en"
-                     ? `Not enough`
-                     : language === "si"
-                     ? "්Not enough"
-                     : "Not enough"}
-                 </h3>
-               </Modal>
+<div>
+      <Modals
+        isModalVisible={isModalVisible}
+        resetFormdata={resetFormdata}
+        handleBackToHome={handleBackToHome}
+        handlePrintReceipt={handlePrintReceipt}
+        language={language}
+        isProErrModalVisible={isProErrModalVisible}
+        setIsProErrModalVisible={setIsProErrModalVisible}
+        isErrModalVisible={isErrModalVisible}
+        setIsErrModalVisible={setIsErrModalVisible}
+        isContErrModalVisible={isContErrModalVisible}
+        setIsContErrModalVisible={setIsContErrModalVisible}
+        isLoyalErrModalVisible={isLoyalErrModalVisible}
+        setIsLoyalErrModalVisible={setIsLoyalErrModalVisible}
+      />
+    </div>
        
       </Content>
     </Layout>  );
