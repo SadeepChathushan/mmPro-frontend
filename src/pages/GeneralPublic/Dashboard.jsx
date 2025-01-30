ff141749b65fc4b9e517025d04e42f8b3ef7021b
Tharushi Hansima <tharushihansima23@gmail.com>
2025-01-30 15:42:03 +0530

    general public dashboard ui modify

--- src/pages/GeneralPublic/Dashboard.jsx
+++ src/pages/GeneralPublic/Dashboard.jsx
@@ -1,12 +1,13 @@
 // src/pages/GeneralPublic/Dashboard.jsx
 import React, { useState, useEffect } from 'react';
 import { useLanguage } from '../../contexts/LanguageContext';
-import logo from '../../assets/images/gsmbLogo.png';
+import logo from '../../assets/images/gsmbLogo.jpg';
 import { submitComplaint } from '../../services/complaint';
 import { fetchLorryNumber } from '../../services/fetchLorryNumber';
 import Modal from '../../components/GeneralPublic/Modal';
 import VehicleInput from '../../components/GeneralPublic/VehicleInput';
 import '../../styles/GeneralPublic/GeneralPublicdashboard.css';
+import backgroundImage from '../../assets/images/generalpublic.jpg';
 
 const Dashboard = () => {
   const { language } = useLanguage();
@@ -62,11 +63,22 @@ const Dashboard = () => {
 
   return (
     <div className="page-container">
-      <header className="header">
-        <img src={logo} alt="Logo" className="header-logo" />
-      </header>
+      <div
+      className="background-section"
+      style={{backgroundImage: `url(${backgroundImage})`}}
+      >
+
+      </div>
+      {/* <header className="header"> */}
+        {/* <img src={logo} alt="Logo" className="header-logo" /> */}
+      {/* </header> */}
       <main className="main-content">
-        <h2 className="title">{language === 'en' ? 'Enter Vehicle Number' : language === 'si' ? 'වාහන අංකය ඇතුලත් කරන්න' : 'வாகன எண்ணை உள்ளிடவும்'}</h2>
+        <header className='header'>
+          <img src={logo} alt='logo' className='header-logo'/>
+
+        </header>
+        <h4 className="title">{language === 'en' ? 'GEOLOGICAL SURVEY & MINES BUREAU' : language === 'si' ? 'භූගෝලීය සමීක්ෂණ සහ පතල් කාර්යාංශය' : 'புவியியல் ஆய்வு மற்றும் சுரங்கப் பணியகம்'}</h4>
+        <p className='para'>{language === 'en'? 'General public users can verify a vehicles validity by entering its vehicle number. The system checks the details against the database and provides an instant result, confirming whether the vehicle is valid or invalid. Additionally, users can submit complaints regarding suspicious or unauthorized vehicles, ensuring better compliance and road safety.':language==='si'?'සාමාන්‍ය පරිශීලකයින්ට වාහනයක වලංගුභාවය එහි වාහන අංකය ඇතුළත් කිරීමෙන් සත්‍යාපනය කළ හැකිය. පද්ධතිය දත්ත සමුදායට අනුව විස්තර පරීක්ෂා කර වාහනය වලංගුද අවලංගුද යන්න තහවුරු කරමින් ක්ෂණික ප්‍රතිඵලයක් ලබා දෙයි. මීට අමතරව, පරිශීලකයින්ට සැක සහිත හෝ අනවසර වාහන සම්බන්ධයෙන් පැමිණිලි ඉදිරිපත් කළ හැකි අතර, එමඟින් වඩා හොඳ අනුකූලතාවයක් සහ මාර්ග ආරක්ෂාව සහතික කෙරේ.':'பொது பயனர்கள் ஒரு வாகனத்தின் செல்லுபடியை அதன் வாகன எண்ணை உள்ளிடுவதன் மூலம் சரிபார்க்கலாம். இந்த அமைப்பு தரவுத்தளத்தில் உள்ள விவரங்களைச் சரிபார்த்து, வாகனம் செல்லுபடியாகுமா அல்லது செல்லாததா என்பதை உறுதிப்படுத்தும் உடனடி முடிவை வழங்குகிறது. கூடுதலாக, பயனர்கள் சந்தேகத்திற்கிடமான அல்லது அங்கீகரிக்கப்படாத வாகனங்கள் தொடர்பான புகார்களைச் சமர்ப்பிக்கலாம், இது சிறந்த இணக்கத்தையும் சாலைப் பாதுகாப்பையும் உறுதி செய்கிறது.'}</p>
         <VehicleInput input={input} setInput={setInput} language={language} />
         <button
           className="check-button"
