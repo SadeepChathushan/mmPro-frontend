b98c2f18e217b01746d9456965ec08c30b6ddb49
Thenuwara <pramukhapoo@gmail.com>
2025-02-22 11:38:58 +0530

    change genaral public

--- src/pages/GeneralPublic/Dashboard.jsx
+++ src/pages/GeneralPublic/Dashboard.jsx
@@ -1,22 +1,27 @@
-import React, { useState, useEffect } from 'react';
-import { useLanguage } from '../../contexts/LanguageContext';
-import logo from '../../assets/images/gsmbLogo.jpg';
-import { submitComplaint } from '../../services/complaint';
-import { fetchLorryNumber } from '../../services/GeneralPublic/fetchLorryNumber';
-import { validateVehicleNumber, validatePhoneNumber } from '../../services/GeneralPublic/validation';
-import Modal from '../../components/GeneralPublic/Modal';
-import VehicleInput from '../../components/GeneralPublic/VehicleInput';
-import '../../styles/GeneralPublic/GeneralPublicdashboard.css';
-import backgroundImage from '../../assets/images/generalpublic.jpg';
+import React, { useState, useEffect } from "react";
+import { useLanguage } from "../../contexts/LanguageContext";
+import logo from "../../assets/images/gsmbLogo.jpg";
+import { submitComplaint } from "../../services/complaint";
+import { fetchLorryNumber } from "../../services/GeneralPublic/fetchLorryNumber";
+import {
+  validateVehicleNumber,
+  validatePhoneNumber,
+} from "../../services/GeneralPublic/validation";
+import Modal from "../../components/GeneralPublic/Modal";
+import VehicleInput from "../../components/GeneralPublic/VehicleInput";
+import "../../styles/GeneralPublic/GeneralPublicdashboard.css";
+import backgroundImage from "../../assets/images/generalpublic.jpg";
+import Header from "../../components/layout/Navbar";
+import Footer from "../../components/layout/Footer";
 
 const Dashboard = () => {
   const { language } = useLanguage();
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState("");
   const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [modalMessage, setModalMessage] = useState('');
+  const [modalMessage, setModalMessage] = useState("");
   const [data, setData] = useState([]);
-  const [phoneNumber, setPhoneNumber] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState("");
 
   useEffect(() => {
     const loadLorryNumbers = async () => {
@@ -28,7 +33,13 @@ const Dashboard = () => {
 
   const handleReport = async () => {
     if (!validatePhoneNumber(phoneNumber)) {
-      setModalMessage(language === 'en' ? 'Invalid Phone Number Format!' : language === 'si'  ? 'වලංගු නොවන දුරකථන අංක ආකෘතියකි!' : 'தவறான தொலைபேசி எண் வடிவம்!');
+      setModalMessage(
+        language === "en"
+          ? "Invalid Phone Number Format!"
+          : language === "si"
+          ? "වලංගු නොවන දුරකථන අංක ආකෘතියකි!"
+          : "தவறான தொலைபேசி எண் வடிவம்!"
+      );
       setIsModalOpen(true);
       return;
     }
@@ -41,37 +52,39 @@ const Dashboard = () => {
   const handleCheck = () => {
     if (!validateVehicleNumber(input)) {
       setModalMessage(
-        language === 'en'
-          ? 'Invalid Vehicle Number Format!'
-          : language === 'si'
-            ? 'අනවශ්‍ය වාහන අංකයක්!'
-            : 'தவறான வாகன எண்ணம்!'
+        language === "en"
+          ? "Invalid Vehicle Number Format!"
+          : language === "si"
+          ? "අනවශ්‍ය වාහන අංකයක්!"
+          : "தவறான வாகன எண்ணம்!"
       );
       setIsModalOpen(true);
       return;
     }
 
-    const validVehicle = data.find((item) => item.vehicleNumber === input.trim());
+    const validVehicle = data.find(
+      (item) => item.vehicleNumber === input.trim()
+    );
 
     setModalMessage(
       validVehicle
-        ? language === 'en'
-          ? 'Valid Load'
-          : language === 'si'
-            ? 'වලංගු පැටවීමකි'
-            : 'சரியான ஏற்றுதல்'
-        : language === 'en'
-          ? 'Invalid Load'
-          : language === 'si'
-            ? 'අනවසර පැටවීමකි'
-            : 'தவறான சுமை'
+        ? language === "en"
+          ? "Valid Load"
+          : language === "si"
+          ? "වලංගු පැටවීමකි"
+          : "சரியான ஏற்றுதல்"
+        : language === "en"
+        ? "Invalid Load"
+        : language === "si"
+        ? "අනවසර පැටවීමකි"
+        : "தவறான சுமை"
     );
     setIsModalOpen(true);
   };
 
   const closeModal = () => {
     setIsModalOpen(false);
-    setPhoneNumber('');
+    setPhoneNumber("");
   };
 
   useEffect(() => {
@@ -79,55 +92,76 @@ const Dashboard = () => {
       setIsMobile(window.innerWidth < 768);
     };
 
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
     return () => {
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener("resize", handleResize);
     };
   }, []);
 
   return (
-    <div className="page-container">
-      <div className="background-section" style={{ backgroundImage: `url(${backgroundImage})` }}></div>
-      <main className="main-content">
-        <header className="header">
-          <img src={logo} alt="logo" className="header-logo" />
-        </header>
-        <h4 className="title">
-          {language === 'en'
-            ? 'GEOLOGICAL SURVEY & MINES BUREAU'
-            : language === 'si'
-              ? 'භූගෝලීය සමීක්ෂණ සහ පතල් කාර්යාංශය'
-              : 'புவியியல் ஆய்வு மற்றும் சுரங்கப் பணியகம்'}
-        </h4>
-        <p className="para">
-          {language === 'en'
-            ? 'General public users can verify a vehicle’s validity by entering its vehicle number. The system checks the details against the database and provides an instant result, confirming whether the vehicle is valid or invalid. Additionally, users can submit complaints regarding suspicious or unauthorized vehicles, ensuring better compliance and road safety.'
-            : language === 'si'
-              ? 'සාමාන්‍ය පරිශීලකයින්ට වාහනයක වලංගුභාවය එහි වාහන අංකය ඇතුළත් කිරීමෙන් සත්‍යාපනය කළ හැකිය...'
-              : 'பொது பயனர்கள் ஒரு வாகனத்தின் செல்லுபடியை அதன் வாகன எண்ணை உள்ளிடுவதன் மூலம் சரிபார்க்கலாம்...'}
-        </p>
-        <VehicleInput input={input} setInput={setInput} language={language} />
-        <button
-          className="check-button"
-          onMouseOver={(e) => (e.target.style.backgroundColor = '#5a0000')}
-          onMouseOut={(e) => (e.target.style.backgroundColor = '#800000')}
-          onClick={handleCheck}
-        >
-          {language === 'en' ? 'Check' : language === 'si' ? 'පරීක්ෂා කරන්න' : 'சரிபார்க்கவும்'}
-        </button>
-      </main>
+    <div
+      style={{
+        display: "flex", // Use flexbox
+        justifyContent: "center", // Center horizontally
+        alignItems: "center", // Center vertically
+        height: "100vh", // Full viewport height (or any desired height)
+      }}
+    >
+      {/* <Header /> */}
+      <div className="page-container">
+        <div
+          className="background-section"
+          style={{ backgroundImage: `url(${backgroundImage})` }}
+        ></div>
+        <main className="main-content">
+          <header className="header">
+            <img src={logo} alt="logo" className="header-logo" />
+          </header>
+          <h4 className="title">
+            {language === "en"
+              ? "GEOLOGICAL SURVEY & MINES BUREAU"
+              : language === "si"
+              ? "භූගෝලීය සමීක්ෂණ සහ පතල් කාර්යාංශය"
+              : "புவியியல் ஆய்வு மற்றும் சுரங்கப் பணியகம்"}
+          </h4>
+          <p className="para">
+            {language === "en"
+              ? "General public users can verify a vehicle’s validity by entering its vehicle number. The system checks the details against the database and provides an instant result, confirming whether the vehicle is valid or invalid. Additionally, users can submit complaints regarding suspicious or unauthorized vehicles, ensuring better compliance and road safety."
+              : language === "si"
+              ? "සාමාන්‍ය පරිශීලකයින්ට වාහනයක වලංගුභාවය එහි වාහන අංකය ඇතුළත් කිරීමෙන් සත්‍යාපනය කළ හැකිය..."
+              : "பொது பயனர்கள் ஒரு வாகனத்தின் செல்லுபடியை அதன் வாகன எண்ணை உள்ளிடுவதன் மூலம் சரிபார்க்கலாம்..."}
+          </p>
+          <VehicleInput input={input} setInput={setInput} language={language} />
+          <button
+            className="check-button"
+            onMouseOver={(e) => (e.target.style.backgroundColor = "#5a0000")}
+            onMouseOut={(e) => (e.target.style.backgroundColor = "#800000")}
+            onClick={handleCheck}
+          >
+            {language === "en"
+              ? "Check"
+              : language === "si"
+              ? "පරීක්ෂා කරන්න"
+              : "சரிபார்க்கவும்"}
+          </button>
+        </main>
 
-      {isModalOpen && (
-        <Modal
-          modalMessage={modalMessage}
-          // phoneNumber={phoneNumber}
-          // setPhoneNumber={setPhoneNumber}
-          // handleReport={handleReport}
-          language={language}
-          closeModal={closeModal}
-          
-        />
-      )}
+        {isModalOpen && (
+          <Modal
+            modalMessage={modalMessage}
+            // phoneNumber={phoneNumber}
+            // setPhoneNumber={setPhoneNumber}
+            // handleReport={handleReport}
+            language={language}
+            closeModal={closeModal}
+          />
+        )}
+      </div>
+      {/* <div style={{
+          marginTop: "60px", // Add margin here
+        }}>
+      <Footer/>
+      </div> */}
     </div>
   );
 };
