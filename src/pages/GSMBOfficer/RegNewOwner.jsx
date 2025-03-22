46ee5df5d3f750437171f289f9ca31baacdb6aef
Janeesha123 <janeeshachathurni2021@gmail.com>
2025-03-22 12:37:31 +0530

    add complaints action check box

--- src/pages/GSMBOfficer/RegNewOwner.jsx
+++ src/pages/GSMBOfficer/RegNewOwner.jsx
@@ -1,16 +1,15 @@
 import React, { useState } from "react";
-import { Form, Input, Button, Row, Col, Upload, message } from "antd";
+import { Form, Input, Button, Row, Col, Upload, message, ConfigProvider } from "antd";
 import { ArrowLeftOutlined, UploadOutlined } from "@ant-design/icons";
 import axios from "axios";
 import uploadMediaToSupabase from "../../utils/imageUpload";
 import { useLanguage } from "../../contexts/LanguageContext";
 import officerService from "../../services/officerService";
-
+import "../../styles/GSMBofficer/gsmbofficer.css"; // Import the same CSS file used by the first component
 
 const NewLicenseForm = () => {
   const { language } = useLanguage();
   const [form] = Form.useForm();
-
 
   const onFinish = async (values) => {
     try {
@@ -19,7 +18,7 @@ const NewLicenseForm = () => {
           login: values.firstname, // Ensure this is unique
           firstname: values.firstname,
           lastname: values.lastname,
-          mail: values.mail,
+          email: values.email,
 
           custom_fields: [
             { id: 21, name: "Mobile Number", value: values.phoneNumber },
@@ -29,7 +28,6 @@ const NewLicenseForm = () => {
         },
       };
 
-  
       const newUser = await officerService.registerUser(userPayload);
       if (!newUser || !newUser.id) {
         throw new Error("User registration failed.");
@@ -37,21 +35,6 @@ const NewLicenseForm = () => {
   
       console.log("User registered successfully:", newUser);
   
-      // // Assign user to GSMB project with ML Owner role
-      // const projectId = 31; // GSMB Project ID
-      // const mlOwnerRoleId = 10; // Ensure this is correct
-  
-      // const membership = await officerService.assignUserToProject(
-      //   newUser.id,
-      //   projectId,
-      //   mlOwnerRoleId
-      // );
-  
-      // if (!membership) {
-      //   throw new Error("Failed to assign user to project.");
-      // }
-  
-      // console.log("User assigned to GSMB project successfully:", membership);
       message.success(
         language === "en"
           ? "Mining License Owner registered successfully!"
@@ -60,7 +43,6 @@ const NewLicenseForm = () => {
           : "சுரங்க அனுமதி உரிமையாளர் வெற்றிகரமாக பதிவு செய்யப்பட்டது!"
       );
   
-
       form.resetFields();
     } catch (error) {
       console.error("Error:", error);
@@ -79,18 +61,18 @@ const NewLicenseForm = () => {
   };
 
   return (
-    <div>
+    <div className="license-form-container">
       <Button
         type="link"
         icon={<ArrowLeftOutlined />}
         style={{ marginBottom: "10px", paddingLeft: 0, color: "#000000" }}
         onMouseEnter={(e) => {
-          e.currentTarget.style.backgroundColor = "#EFE29C"; // Hover color
-          e.currentTarget.style.borderColor = "#EFE29C"; // Hover border
+          e.currentTarget.style.backgroundColor = "#EFE29C";
+          e.currentTarget.style.borderColor = "#EFE29C";
         }}
         onMouseLeave={(e) => {
-          e.currentTarget.style.backgroundColor = "#ffffff"; // Default color
-          e.currentTarget.style.borderColor = "#ffffff"; // Default border
+          e.currentTarget.style.backgroundColor = "#ffffff";
+          e.currentTarget.style.borderColor = "#ffffff";
         }}
         href="/gsmb/dashboard"
       >
@@ -111,219 +93,204 @@ const NewLicenseForm = () => {
           ? "නව බලපත්‍ර අයිතිකරු ලියාපදිංචි කරන්න"
           : "புதிய அனுமதி உரிமையாளரை பதிவு செய்"}
       </h2>
-      <Form form={form} layout="vertical" onFinish={onFinish}>
-        <Row gutter={[16, 16]}>
-          <Col xs={24} sm={24} md={12}>
-            <Form.Item
-              style={{ fontSize: "32px" }}
-
-              label={language === "en" ? "First Name" : language === "si" ? "අයිතිකරුගේ මුල් නම" : "உரிமையாளர் முதற்கொண்டு பெயர்"}
-              name="firstname"
-
-              rules={[
-                {
-                  required: true,
-                  message:
-                    language === "en"
-
-                      ? "Please input the first name!"
-                      : language === "si"
-                      ? "අයිතිකරුගේ මුල් නම ඇතුළත් කරන්න!"
-                      : "உரிமையாளர் முதற்கொண்டு பெயரை உள்ளிடவும்!",
-
-                },
-              ]}
-            >
-              <Input style={{ fontSize: "24px" }} />
-            </Form.Item>
-          </Col>
-
-
-          <Col xs={24} sm={24} md={12}>
-            <Form.Item
-
-              style={{ fontSize: "32px" }}
-              label={language === "en" ? "Last Name" : language === "si" ? "අයිතිකරුගේ අවසන් නම" : "உரிமையாளர் கடைசி பெயர்"}
-              name="lastname"
-
-              rules={[
-                {
-                  required: true,
-                  message:
-                    language === "en"
-
-                      ? "Please input the last name!"
-                      : language === "si"
-                      ? "අයිතිකරුගේ අවසන් නම ඇතුළත් කරන්න!"
-                      : "உரிமையாளர் கடைசி பெயரை உள்ளிடவும்!",
-
-                },
-              ]}
-            >
-              <Input style={{ fontSize: "24px" }} />
-            </Form.Item>
-          </Col>
-
-          <Col xs={24} sm={24} md={12}>
-            <Form.Item
-
-              label={language === "en" ? "Email Address" : language === "si" ? "විද්‍යුත් තැපෑල" : "மின்னஞ்சல் முகவரி"}
-              name="mail"
-
-              rules={[
-                {
-                  required: true,
-                  message:
-                    language === "en"
-
-                      ? "Please input the Email!"
-                      : language === "si"
-                      ? "විද්‍යුත් තැපෑල ඇතුළත් කරන්න!"
-                      : "மின்னஞ்சலை உள்ளிடவும்!",
-
-                },
-              ]}
-            >
-              <Input style={{ fontSize: "24px" }} />
-            </Form.Item>
-          </Col>
-
-          <Col xs={24} sm={24} md={12}>
-            <Form.Item
-
-              label={language === "en" ? "Mobile Number" : language === "si" ? "ජංගම දුරකථන අංකය" : "மொபைல் எண்"}
-              name="phoneNumber"
-
-              rules={[
-                {
-                  required: true,
-                  message:
-                    language === "en"
-
-                      ? "Please input the mobile number!"
-                      : language === "si"
-                      ? "ජංගම දුරකථන අංකය ඇතුළත් කරන්න!"
-                      : "மொபைல் எண்ணை உள்ளிடவும்!",
-
-                },
-              ]}
-            >
-              <Input style={{ fontSize: "24px" }} />
-            </Form.Item>
-          </Col>
-         
-          <Col xs={24} sm={24} md={12}>
-            <Form.Item
-              label={language === "en" ? "Address" : language === "si" ? "ලිපිනය" : "முகவரி"}
-              name="address"
-              rules={[
-                {
-                  required: true,
-                  message:
-                    language === "en"
-                      ? "Please input the address!"
-                      : language === "si"
-                      ? "ලිපිනය ඇතුළත් කරන්න!"
-                      : "முகவரியை உள்ளிடவும்!",
-                },
-              ]}
-            >
-              <Input style={{ fontSize: "24px" }} />
-            </Form.Item>
-          </Col>
-
-          <Col xs={24} sm={24} md={12}>
-            <Form.Item
-              label={language === "en" ? "NIC" : language === "si" ? "ජාතික හැඳුනුම්පත" : "தேசிய அடையாளம்"}
-              name="NIC"
-
-              rules={[
-                {
-                  required: true,
-                  message:
-                    language === "en"
-
-                      ? "Please input the NIC Number!"
-                      : language === "si"
-                      ? "ජාතික හැඳුනුම්පත ඇතුළත් කරන්න!"
-                      : "தேசிய அடையாள எண்ணை உள்ளிடவும்!",
-                },
-              ]}
-            >
-              <Input style={{ fontSize: "24px" }} />
-
-            </Form.Item>
-          </Col>
-
-          <Col xs={24}>
-            <Form.Item>
-              <div
-                style={{
-                  marginLeft: "10px",
-                  display: "flex",
-                  flexDirection: "row",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  gap: "50px",
-                 
-
-                }}
+      
+      <ConfigProvider 
+        theme={{ 
+          token: {
+            fontSize: 18,
+            colorText: "black",
+            colorPrimary: "#950C33",
+            colorTextPlaceholder: "rgba(0, 0, 0, 0.45)",
+            colorTextDisabled: "rgba(0, 0, 0, 0.65)",
+          },
+          components: { 
+            Input: { 
+              colorText: "black",
+              colorTextDisabled: "rgba(0, 0, 0, 0.65)",
+              colorTextPlaceholder: "rgba(0, 0, 0, 0.45)",
+            },
+            Form: {
+              colorTextLabel: "black",
+            }
+          } 
+        }}
+      >
+        <Form 
+          form={form} 
+          layout="vertical" 
+          onFinish={onFinish}
+          className="black-text-form"
+        >
+          <Row gutter={[16, 16]}>
+            <Col xs={24} sm={24} md={12}>
+              <Form.Item
+                label={language === "en" ? "First Name" : language === "si" ? "අයිතිකරුගේ මුල් නම" : "உரிமையாளர் முதற்கொண்டு பெயர்"}
+                name="firstname"
+                rules={[
+                  {
+                    required: true,
+                    message:
+                      language === "en"
+                        ? "Please input the first name!"
+                        : language === "si"
+                        ? "අයිතිකරුගේ මුල් නම ඇතුළත් කරන්න!"
+                        : "உரிமையாளர் முதற்கொண்டு பெயரை உள்ளிடவும்!",
+                  },
+                ]}
               >
-                <Button
-                  type="primary"
-                  htmlType="submit"
+                <Input className="text-input" style={{ color: "black", fontSize: "20px" }} />
+              </Form.Item>
+            </Col>
+
+            <Col xs={24} sm={24} md={12}>
+              <Form.Item
+                label={language === "en" ? "Last Name" : language === "si" ? "අයිතිකරුගේ අවසන් නම" : "உரிமையாளர் கடைசி பெயர்"}
+                name="lastname"
+                rules={[
+                  {
+                    required: true,
+                    message:
+                      language === "en"
+                        ? "Please input the last name!"
+                        : language === "si"
+                        ? "අයිතිකරුගේ අවසන් නම ඇතුළත් කරන්න!"
+                        : "உரிமையாளர் கடைசி பெயரை உள்ளிடவும்!",
+                  },
+                ]}
+              >
+                <Input className="text-input" style={{ color: "black", fontSize: "20px" }} />
+              </Form.Item>
+            </Col>
+
+            <Col xs={24} sm={24} md={12}>
+              <Form.Item
+                label={language === "en" ? "Email Address" : language === "si" ? "විද්‍යුත් තැපෑල" : "மின்னஞ்சல் முகவரி"}
+                name="email"
+                rules={[
+                  {
+                    required: true,
+                    message:
+                      language === "en"
+                        ? "Please input the Email!"
+                        : language === "si"
+                        ? "විද්‍යුත් තැපෑල ඇතුළත් කරන්න!"
+                        : "மின்னஞ்சலை உள்ளிடவும்!",
+                  },
+                ]}
+              >
+                <Input className="text-input" style={{ color: "black", fontSize: "20px" }} />
+              </Form.Item>
+            </Col>
+
+            <Col xs={24} sm={24} md={12}>
+              <Form.Item
+                label={language === "en" ? "Mobile Number" : language === "si" ? "ජංගම දුරකථන අංකය" : "மொபைல் எண்"}
+                name="phoneNumber"
+                rules={[
+                  {
+                    required: true,
+                    message:
+                      language === "en"
+                        ? "Please input the mobile number!"
+                        : language === "si"
+                        ? "ජංගම දුරකථන අංකය ඇතුළත් කරන්න!"
+                        : "மொபைல் எண்ணை உள்ளிடவும்!",
+                  },
+                ]}
+              >
+                <Input className="text-input" style={{ color: "black", fontSize: "20px" }} />
+              </Form.Item>
+            </Col>
+           
+            <Col xs={24} sm={24} md={12}>
+              <Form.Item
+                label={language === "en" ? "Address" : language === "si" ? "ලිපිනය" : "முகவரி"}
+                name="address"
+                rules={[
+                  {
+                    required: true,
+                    message:
+                      language === "en"
+                        ? "Please input the address!"
+                        : language === "si"
+                        ? "ලිපිනය ඇතුළත් කරන්න!"
+                        : "முகவரியை உள்ளிடவும்!",
+                  },
+                ]}
+              >
+                <Input className="text-input" style={{ color: "black", fontSize: "20px" }} />
+              </Form.Item>
+            </Col>
+
+            <Col xs={24} sm={24} md={12}>
+              <Form.Item
+                label={language === "en" ? "NIC" : language === "si" ? "ජාතික හැඳුනුම්පත" : "தேசிய அடையாளம்"}
+                name="NIC"
+                rules={[
+                  {
+                    required: true,
+                    message:
+                      language === "en"
+                        ? "Please input the NIC Number!"
+                        : language === "si"
+                        ? "ජාතික හැඳුනුම්පත ඇතුළත් කරන්න!"
+                        : "தேசிய அடையாள எண்ணை உள்ளிடவும்!",
+                  },
+                ]}
+              >
+                <Input className="text-input" style={{ color: "black", fontSize: "20px" }} />
+              </Form.Item>
+            </Col>
+
+            <Col xs={24}>
+              <Form.Item>
+                <div
                   style={{
-                    flex: "1 1 48%",
-                    maxWidth: "300px",
-                    // width: "48%",
-                    backgroundColor: "#950C33",
-                    borderColor: "#950C33",
-                    height: "40px",
-                  }}
-                  onMouseEnter={(e) => {
-                    e.currentTarget.style.backgroundColor = "#FFE143"; // Hover color
-                    e.currentTarget.style.borderColor = "#FFE143"; // Hover border
-                  }}
-                  onMouseLeave={(e) => {
-                    e.currentTarget.style.backgroundColor = "#950C33"; // Default color
-                    e.currentTarget.style.borderColor = "#950C33"; // Default border
+                    marginLeft: "10px",
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    gap: "50px",
                   }}
                 >
-
-                  {language == "en"
-                    ? "Create"
-                    : language === "si"
-                    ? "සාදන්න"
-                    : "உருவாக்க"}
-
-                </Button>
-                <Button
-                  type="default"
-                  onClick={handleCancel}
-                  style={{
-                    flex: "1 1 48%",
-                    maxWidth: "300px",
-                    // width: "48%",
-                    borderColor: "#950C33",
-                    backgroundColor: "#FFFFFF",
-                    height: "40px",
-                  }}
-                  onMouseEnter={(e) => {
-                    e.currentTarget.style.backgroundColor = "#950C33"; // Hover color
-                    e.currentTarget.style.borderColor = "#950C33"; // Hover border
-                  }}
-                  onMouseLeave={(e) => {
-                    e.currentTarget.style.backgroundColor = "#FFFFFF"; // Default color
-                    e.currentTarget.style.borderColor = "#950C33"; // Default border
-                  }}
-                >
-                  {language == "en" ? "Cancel" : language === "si" ? "අවලංගු කරන්න" : "ரத்து செய்"}
-                </Button>
-              </div>
-            </Form.Item>
-          </Col>
-        </Row>
-      </Form>
+                  <Button
+                    type="primary"
+                    htmlType="submit"
+                    style={{
+                      flex: "1 1 48%",
+                      maxWidth: "300px",
+                      backgroundColor: "#950C33",
+                      borderColor: "#950C33",
+                      height: "40px",
+                    }}
+                  >
+                    {language === "en"
+                      ? "Create"
+                      : language === "si"
+                      ? "සාදන්න"
+                      : "உருவாக்க"}
+                  </Button>
+                  <Button
+                    type="default"
+                    onClick={handleCancel}
+                    style={{
+                      flex: "1 1 48%",
+                      maxWidth: "300px",
+                      backgroundColor: "#FFFFFF",
+                      borderColor: "#950C33",
+                      height: "40px",
+                      color: "black",
+                    }}
+                  >
+                    {language === "en" ? "Cancel" : language === "si" ? "අවලංගු කරන්න" : "ரத்து செய்"}
+                  </Button>
+                </div>
+              </Form.Item>
+            </Col>
+          </Row>
+        </Form>
+      </ConfigProvider>
     </div>
   );
 };
