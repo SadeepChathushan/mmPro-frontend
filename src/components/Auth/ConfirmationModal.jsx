917d9f9dd7bf0e65ccfb0f0a71039370007a5732
Dilmi Ranaweera <dilmiranaweera1999@gmail.com>
2025-03-26 13:57:41 +0530

    latest

--- src/components/Auth/ConfirmationModal.jsx
+++ src/components/Auth/ConfirmationModal.jsx
@@ -1,5 +1,6 @@
-import { Modal, Button } from "antd";
-import { ArrowRightOutlined } from "@ant-design/icons"; // Import the arrow icon
+import PropTypes from 'prop-types';
+import { Modal, Button, message } from "antd";
+import { ArrowRightOutlined } from "@ant-design/icons";
 import emailIcon from "../../assets/images/check_email.png";
 import "../../styles/forgotpassword.css";
 
@@ -18,40 +19,45 @@ const ConfirmationModal = ({ visible, onClose }) => {
       centered
       className="confirmation-modal"
     >
-    <div className="modal-container">
+      <div className="modal-container">
         <div className="icon-container">
-        <img src={emailIcon} alt="Forgot Password Icon" className="fp-icon" />
-      </div>
+          <img src={emailIcon} alt="Forgot Password Icon" className="fp-icon" />
+        </div>
 
-      <div className="modal-title">Check Your Email !</div>
+        <div className="modal-title">Check Your Email !</div>
 
-      <div className="modal-p">
-        Thanks! An email was sent that will ask you to click on a link to verify
-        that you own this account.
-      </div>
+        <div className="modal-p">
+          Thanks! An email was sent that will ask you to click on a link to verify
+          that you own this account.
+        </div>
 
-      <div style={{ textAlign: "center", marginTop: "20px" }}>
-        <Button
-          type="primary"
-          size="large"
-          className="submit-button"
-          style={{ marginBottom: "10px" }}
-        >
-          Open Email Inbox
-        </Button>
-        <Button
-          type="default"
-          size="large"
-          className="resend-button"
-          onClick={handleResendEmail}
-          icon={<ArrowRightOutlined />}
-        >
-          Resend Email
-        </Button>
-      </div>
+        <div style={{ textAlign: "center", marginTop: "20px" }}>
+          <Button
+            type="primary"
+            size="large"
+            className="submit-button"
+            style={{ marginBottom: "10px" }}
+          >
+            Open Email Inbox
+          </Button>
+          <Button
+            type="default"
+            size="large"
+            className="resend-button"
+            onClick={handleResendEmail}
+            icon={<ArrowRightOutlined />}
+          >
+            Resend Email
+          </Button>
+        </div>
       </div>
     </Modal>
   );
+};
+
+ConfirmationModal.propTypes = {
+  visible: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired
 };
 
 export default ConfirmationModal;
